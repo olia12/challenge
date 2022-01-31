acb54a95177aff787125fd71291e6fd5159a689a
Popova Olga <686371olia@mail.ru>
2022-02-01 00:54:36 +0300

    Возведение в степень, вывод первых двух цифр числа

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ let num = 266219;
 num = String(num);
 let numLength = num.length;
 let product = 1;
+let degree;
 
 for (let i = 0; i < numLength; i++) {
   product *= num[i];
 }
 console.log(product);
+
+degree = product ** 3;
+degree = String(degree);
+console.log(degree.substring(0, 2));
