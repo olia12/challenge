d5c1c5c86f6d09f8ba983a37de7da1ddd8a48330
Popova Olga <686371olia@mail.ru>
2022-02-04 00:35:38 +0300

    Вывод дней недели при помощи конструкции switch-case

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 "use strict";
 let lang = prompt("Выбирете язык: ru или en");
 
-if (lang == "ru") {
-  console.log(
-    "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-  );
-} else if (lang == "en") {
-  console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
-} else {
-  console.log("Неверно выбран язык");
+// if (lang == "ru") {
+//   console.log(
+//     "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+//   );
+// } else if (lang == "en") {
+//   console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
+// } else {
+//   console.log("Неверно выбран язык");
+// }
+
+switch (true) {
+  case lang == "ru":
+    console.log(
+      "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+    );
+    break;
+  case lang == "en":
+    console.log(
+      "Monday, tuesday, wednesday, thursday, friday, saturday, sunday"
+    );
+    break;
+  case lang !== "ru" && lang !== "en":
+    console.log("Неверно выбран язык");
 }
