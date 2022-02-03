dda4f017a768d030f41532374419f8a599d598c2
Popova Olga <686371olia@mail.ru>
2022-02-04 01:17:22 +0300

    Вывод дней недели при помощи  многомерного массива

--- script.js
+++ script.js
@@ -11,17 +11,23 @@ let lang = prompt("Выбирете язык: ru или en");
 //   console.log("Неверно выбран язык");
 // }
 
-switch (true) {
-  case lang == "ru":
-    console.log(
-      "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-    );
-    break;
-  case lang == "en":
-    console.log(
-      "Monday, tuesday, wednesday, thursday, friday, saturday, sunday"
-    );
-    break;
-  case lang !== "ru" && lang !== "en":
-    console.log("Неверно выбран язык");
-}
+// switch (true) {
+//   case lang == "ru":
+//     console.log(
+//       "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+//     );
+//     break;
+//   case lang == "en":
+//     console.log(
+//       "Monday, tuesday, wednesday, thursday, friday, saturday, sunday"
+//     );
+//     break;
+//   case lang !== "ru" && lang !== "en":
+//     console.log("Неверно выбран язык");
+// }
+
+let array = {
+  ru: "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье",
+  en: "Monday, tuesday, wednesday, thursday, friday, saturday, sunday",
+};
+console.log(array[lang]);
