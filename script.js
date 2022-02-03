36f517ed01269d96c86682fbb6420c2850fc851d
Popova Olga <686371olia@mail.ru>
2022-02-04 01:51:41 +0300

    Задача с использованием нескольких тернарных операторов

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-"use strict";
-let lang = prompt("Выбирете язык: ru или en");
+'use strict';
+ let lang = prompt("Выбирете язык: ru или en");
 
-// if (lang == "ru") {
-//   console.log(
-//     "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-//   );
-// } else if (lang == "en") {
-//   console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
-// } else {
-//   console.log("Неверно выбран язык");
-// }
+// 1.
+// a)
+if (lang == "ru") {
+  console.log(
+    "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+  );
+} else if (lang == "en") {
+  console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
+} else {
+  console.log("Неверно выбран язык");
+}
 
-// switch (true) {
-//   case lang == "ru":
-//     console.log(
-//       "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-//     );
-//     break;
-//   case lang == "en":
-//     console.log(
-//       "Monday, tuesday, wednesday, thursday, friday, saturday, sunday"
-//     );
-//     break;
-//   case lang !== "ru" && lang !== "en":
-//     console.log("Неверно выбран язык");
-// }
+// b)
+switch (true) {
+  case lang == "ru":
+    console.log(
+      "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+    );
+    break;
+  case lang == "en":
+    console.log(
+      "Monday, tuesday, wednesday, thursday, friday, saturday, sunday"
+    );
+    break;
+  case lang !== "ru" && lang !== "en":
+    console.log("Неверно выбран язык");
+}
 
+// c)
 let array = {
   ru: "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье",
   en: "Monday, tuesday, wednesday, thursday, friday, saturday, sunday",
 };
 console.log(array[lang]);
+
+//2
+let namePerson = prompt("Введите ваше имя");
+let position = (namePerson == "Александр") ? console.log("директор"): 
+(namePerson == "Артем") ? console.log("преподаватель"):
+console.log("студент");
